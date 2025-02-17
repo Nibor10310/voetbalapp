907cf05820f466349828ceacb8fb49d3f02dee32
Nibor10310 <robin@bizantium.be>
2025-02-17 11:28:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensures Vercel correctly finds index.html
   build: {
-    outDir: 'dist',
     rollupOptions: {
       input: 'index.html', // Ensures Vite finds the entry file
     },
+    outDir: 'dist'
   },
 });
